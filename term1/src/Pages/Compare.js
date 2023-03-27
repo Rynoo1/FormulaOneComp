c09dee2b8e9b98ce317744244a1100db94cd644e
Rynoo1 <221361@virtualwindow.co.za>
2023-03-27 15:09:17 +0200

    Update to timeline page and compare page
    Work on timeline page
    Work on compare page
    Installed chartjs

--- term1/src/Pages/Compare.js
+++ term1/src/Pages/Compare.js
@@ -1,34 +1,34 @@
-import React from 'react'
-//import axios from "axios";
+import React, { useRef, useState } from 'react'
+import axios from "axios";
 
 export default function Compare() {
-  // const options = {
-  //   method: 'GET',
-  //   url: 'https://api-formula-1.p.rapidapi.com/drivers',
-  //   params: { search: 'lewis' },
-  //   headers: {
-  //     'X-RapidAPI-Key': '8ef8fc27c6msh7504b9ec3178fa1p1a705fjsnfb988f103d4a',
-  //     'X-RapidAPI-Host': 'api-formula-1.p.rapidapi.com'
-  //   }
-  // };
+  const options = {
+    method: 'GET',
+    url: 'https://api-formula-1.p.rapidapi.com/drivers',
+    params: { search: 'lewis' },
+    headers: {
+      'X-RapidAPI-Key': '8ef8fc27c6msh7504b9ec3178fa1p1a705fjsnfb988f103d4a',
+      'X-RapidAPI-Host': 'api-formula-1.p.rapidapi.com'
+    }
+  };
 
-  // axios.request(options).then(function (response) {
-  //   console.log(response.data);
-  // }).catch(function (error) {
-  //   console.error(error);
-  // });
+  axios.request(options).then(function (response) {
+    console.log(response.data);
+  }).catch(function (error) {
+    console.error(error);
+  });
 
   return (
     <div class="row p-3">
 
-      <div class="col-6">
+      <div class="w-50">
         <label for="driver1label" class="form-label">Driver One</label>
-        <input type="text" class="form-control" id="driver1name" placeholder="Max Verstappen"></input>
+        <input type="text" class="form-control" id="driver1name" placeholder='Max Verstappen'/>
         <label for="season1label" class="form-label">Season/Year</label>
         <input type="text" class="form-control" id="driver1season" placeholder="2022"></input>
       </div>
 
-      <div class="col-6">
+      <div class="w-50">
         <label for="driver2label" class="form-label">Driver Two</label>
         <input type="text" class="form-control" id="driver2name" placeholder="Sergio Perez"></input>
         <label for="season2label" class="form-label">Season/Year</label>
--- term1/src/Pages/Time.js
+++ term1/src/Pages/Time.js
@@ -1,7 +1,48 @@
 import React from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import LineChart from '../Components/LineChart';
+
 
 export default function Time() {
+
+  const [seasonData, setSeasonData] = useState();
+
+  const [driverData, setDriverData] = useState({
+    labels: seasonData.driver.name,
+    datasets: [{
+      label: 'Season Points',
+      data: seasonData.points,
+      tension: 0.4
+    }]
+  });
+
+
+  const options = {
+    method: 'GET',
+    url: 'https://api-formula-1.p.rapidapi.com/rankings/drivers',
+    params: { season: '2022' },
+    headers: {
+      'X-RapidAPI-Key': '8ef8fc27c6msh7504b9ec3178fa1p1a705fjsnfb988f103d4a',
+      'X-RapidAPI-Host': 'api-formula-1.p.rapidapi.com'
+    }
+  };
+
+  useEffect(() => {
+    axios.request(options).then(function (response) {
+      setSeasonData(response.data.response);
+      console.log(seasonData);
+    }).catch(function (error) {
+      console.error(error);
+    });
+  }, [])
+
+
   return (
-    <div>Time</div>
+    <div>
+      <LineChart ChartData={ driverData } />
+      hi
+    </div>
+
   )
 }
